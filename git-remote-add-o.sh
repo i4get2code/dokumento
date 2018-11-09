deaf2549df86341d59fa43e176eccd70cdcc8462
anonymous anonymous <not4get2read@yandex>
2018-11-09 04:10:43 +0200

    -o

--- git-remote-add-o.sh
+++ git-remote-add-o.sh
@@ -1,3 +1,6 @@
+git remote set-url --push off https://github.com/i4get2code/dokumento.git
+git remote set-url --add --push off ../dokumento.git
+
 git remote set-url on https://github.com/i4get2code/dokumento.git
 git remote set-url --push on ../dokumento.git
 git remote set-url --add --push on https://github.com/i4get2code/dokumento.git
--- git-remote-add-off.sh
+++
@@ -1,3 +0,0 @@
-git remote set-url --push off https://github.com/i4get2code/dokumento.git
-git remote set-url --add --push off ../dokumento.git
-
